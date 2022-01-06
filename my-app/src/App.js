d656d8d94b3b6516bd639b9f80a07ace227fc9d1
ISHANI MISHRA <71793856+Ishani-Mishra@users.noreply.github.com>
2022-01-06 13:37:49 +0530

    Add files via upload

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import Intro from "../src/components/Intro/Intro"
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Intro></Intro>
+    </div>
+  );
+}
+
+export default App;             
