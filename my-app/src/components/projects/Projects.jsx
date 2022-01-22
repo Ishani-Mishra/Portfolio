36b521ba234c4a95219ac3460885de79fc4cd8af
unknown <ishani1407@gmail.com>
2022-01-22 19:33:10 +0530

    gear

--- my-app/src/components/projects/Projects.jsx
+++ my-app/src/components/projects/Projects.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import "./projects.css"
 
+
+
 export default function Projects() {
-  return <></>;
+  return (
+  <div className='box'>
+    <div className="gears"></div>
+    <div className="pro-1"></div>
+    <div className="pro-2"></div>
+    <div className="pro-3"></div>
+  </div>);
 }
