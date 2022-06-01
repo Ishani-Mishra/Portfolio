191d394af6ae852b26e81d5a618c32544d71e49a
unknown <ishani1407@gmail.com>
2022-06-02 04:52:06 +0530

    animations

--- my-app/src/components/Navbar/Navbar.jsx
+++ my-app/src/components/Navbar/Navbar.jsx
@@ -6,35 +6,14 @@ function Navbar() {
     return (
         <>
           <div className="navbar">
-            <div className="home nav-item">
-                <Link className="Link" to={'/'}>
-                    HOME
-                </Link>
-            </div>
-            <div className="projects nav-item">
-                <Link className="Link" to={'/projects'}>
-                    PROJECTS
-                </Link>
-            </div>
-            <div className="getintouch nav-item">
-                <Link className="Link" to={'/get-in-touch'}>
-                    GET IN TOUCH
-                </Link>
-            </div>
-            <div className="publication nav-item">
-                <Link className="Link" to={'/publication'}>
-                    PUBLICATION
-                </Link>
-            </div>
-            <div className="blogs nav-item">
-                <Link className="Link" to={'/blogs'}>
-                    BLOGS
-                </Link>
-            </div>
+            <div className="home nav-item"><Link className="Link" to={'/'}>HOME</Link></div>
+            <div className="projects nav-item"><Link className="Link" to={'/projects'}>PROJECTS</Link></div>
+            <div className="getintouch nav-item"><Link className="Link" to={'/get-in-touch'}>GET IN TOUCH</Link></div>
+            <div className="publication nav-item"><Link className="Link" to={'/publication'}>PUBLICATION</Link></div>
+            <div className="blogs nav-item"><Link className="Link" to={'/blogs'}>BLOGS</Link></div>
           </div>  
         </>
     )
 }
-
 export default Navbar
 
--- my-app/src/components/projects/Projects.jsx
+++ my-app/src/components/projects/Projects.jsx
@@ -1,14 +1,87 @@
 import React from 'react';
 import "./projects.css"
-
+import Project from "../project/Project"
 
 
 export default function Projects() {
   return (
   <div className='box'>
     <div className="gears"></div>
-    <div className="pro-1"></div>
-    <div className="pro-2"></div>
-    <div className="pro-3"></div>
+    <div className="pro-1"><Project /></div>
+    <div className="pro-2"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
+    <div className="pro-3"><Project /></div>
   </div>);
 }
